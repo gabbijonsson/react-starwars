1ae4adea9088ca8321ee7e37176d11fa3db52701
Gabrielle <dev.gabrielle.jonsson@gmail.com>
2020-06-01 15:26:51 +0200

    Updates

---
+++ precache-manifest.cd9353c7e8a4dfc7b1db487a0a30d522.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e8c76af0322ab511f3153824b16b6e",
+    "url": "/react-starwars/index.html"
+  },
+  {
+    "revision": "cc3728846cc508a88676",
+    "url": "/react-starwars/static/css/main.485fe1c4.chunk.css"
+  },
+  {
+    "revision": "03296e59ed6872823284",
+    "url": "/react-starwars/static/js/2.efeffa55.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/react-starwars/static/js/2.efeffa55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc3728846cc508a88676",
+    "url": "/react-starwars/static/js/main.712c1201.chunk.js"
+  },
+  {
+    "revision": "bc827e72e0238276acdc",
+    "url": "/react-starwars/static/js/runtime-main.87c7810d.js"
+  },
+  {
+    "revision": "3f632548770ccc335c9148c25b9d1e7a",
+    "url": "/react-starwars/static/media/HeaderSWAPI.3f632548.svg"
+  }
+]);
